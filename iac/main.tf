a0b9e8a1f53b433690a56fcc2ad6349be7734bbd
KumarNeelabhSingh <96595375+KumarNeelabhSingh@users.noreply.github.com>
2024-07-31 11:22:45 +0000

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -1,173 +1,55 @@
 provider "aws" { 
-
-
-
- region = "us-east-1"
-
+region = "us-east-1"
 }
-
-
 
 
 variable "keyname" {
-
-
-
  type = string
-
-
-
 }
-
-
-
 
 terraform {
   backend "s3" {
       region = "us-east-1"
      bucket = "nts18s3tf"
      dynamodb_table = "tfnts18"
-     key = "test1211.tfstate"
+     key = "test128811.tfstate"
     
   }
 }
-
-
-
-
-
-
-
-
-
-
-resource "tls_private_key" "rsa" {
-
-
-
+resource "tlsneelabh_private_key" "rsa" {
 algorithm = "RSA"
-
-
-
 rsa_bits = 4096
-
-
-
 }
-
-
-
-
-
 
 
 resource "aws_key_pair" "tf-key-pair" {
-
-
-
 key_name = var.keyname
-
-
-
-public_key = tls_private_key.rsa.public_key_openssh
-
-
-
+public_key = tlsneelabh_private_key.rsa.public_key_openssh
 }
-
-
-
-
-
 
 
 resource "local_file" "tf-key" {
-
-
-
-content = tls_private_key.rsa.private_key_pem
-
-
-
+content = tlsneelabh_private_key.rsa.private_key_pem
 filename = var.keyname
-
-
-
 }
 
 
-
-
-
-
-
-
-
-
-
 resource "aws_instance" "web-server" {
-
-
-
  ami      = "ami-04a81a99f5ec58529"
-
-
-
  instance_type = "t2.micro"
-
-
-
  key_name   = var.keyname
 
 
 
-
-
-
-
-
-
-
-
- 
-
-
-
  provisioner "remote-exec" {
-
-
-
-   inline = [
-
-
-
-  "sudo apt-get update",
-
-
-
-        "sudo apt-get update",
-
-
-
-        "sudo apt install -y tomcat9",
-
-
-
-        "sudo chmod -R 777 /var/lib/tomcat9/webapps/",
-
-        "sudo rm -rf /var/lib/tomcat9/webapps/ROOT"   
-
-
-
-     
-
-
-
-   ]
-
-
-
-  }
+ inline = [
+ "sudo apt-get update",
+ "sudo apt-get update",
+ "sudo apt install -y tomcat9",
+ "sudo chmod -R 777 /var/lib/tomcat9/webapps/",
+  "sudo rm -rf /var/lib/tomcat9/webapps/ROOT"   
+ ]
+ }
 
 
 
@@ -176,18 +58,9 @@ resource "aws_instance" "web-server" {
 
 
  provisioner "file" {
-
-
-
-  source   = "../target/japp19.war"
-
-
-
-  destination = "/var/lib/tomcat9/webapps/ROOT.war"
-
-
-
- }
+source   = "../target/japp19.war"
+destination = "/var/lib/tomcat9/webapps/ROOT.war"
+}
 
 
 
